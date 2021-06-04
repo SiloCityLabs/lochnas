e52c12835cd42ee5939c7d8b1f113e0c14984122
Luis Rodriguez <luis@silocitylabs.com>
2021-06-04 00:50:26 -0400

    update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Root check
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+cd "$(dirname "$0")"
+
+apt-get update && apt -y dist-upgrade
+
+git pull
