6a8561c1aef0a86e4ef62b3ab841075bb2bfc8a8
Luis Rodriguez <luis@techreanimate.com>
2021-06-22 23:03:01 -0400

    Update domain-remove.sh

--- domain-remove.sh
+++ domain-remove.sh
@@ -8,4 +8,4 @@ source .env
 docker run --rm -i \
 -v "${PERSISTENT_ROOT}/docker-data/letsencrypt:/etc/letsencrypt" \
 -v "${PERSISTENT_ROOT}/docker-data/certbot:/var/www/certbot" \
-certbot/certbot 'delete'
+certbot/certbot:arm64v8-latest 'delete'
