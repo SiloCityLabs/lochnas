5d2c724aa349eff19bda34ce2b596cb367c16755
Luis Rodriguez <luis@techreanimate.com>
2021-10-08 20:57:15 -0400

    Update getip.sh
    Fix

--- scripts/getip.sh
+++ scripts/getip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wanip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+wanip="$(dig +short myip.opendns.com @resolver4.opendns.com)"
 hostname="$(hostname)"
 
 # Check if domain points here
 checkdomain() {
-    dnsip="$(dig +short $1.$ROOT_DOMAIN @resolver1.opendns.com)"
+    dnsip="$(dig +short $1.$ROOT_DOMAIN @resolver4.opendns.com)"
     if [[ ! $wanip == $dnsip ]]; then
         echo "$1 is not pointing to your ip"
         exit 1
