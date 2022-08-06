9866d71495af5211347d5f600462a0000e8f32c9
Luis Rodriguez <luis@silocitylabs.com>
2022-08-06 15:28:32 -0400

    domain add and delete

--- server/models/domain.go
+++ server/models/domain.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"os"
 	"server/util"
 )
@@ -49,16 +50,49 @@ func (a DomainModel) Renew() string {
 func (a DomainModel) Add() string {
 	args := os.Args[1:]
 
-	if len(args) != 1 {
+	fmt.Println(args)
+
+	if len(args) != 3 {
 		return "Please supply only one domain name like: './server.bin -domain add domain.com'"
 	}
 
-	// util.Command(false, Config.WorkingDirectory, nil, "./scripts/domain-add.sh "+args[0])
+	// Check if nginx container is running in docker
+	output, err := util.Command(false, Config.WorkingDirectory, nil, "docker ps -aq -f status=running -f name=nginx")
+	if err != nil {
+		return "Please stop nginx container in docker"
+	}
+
+	// nginx is running if the output is not empty, lets stop nginx container
+	if output != "" {
+		util.Command(false, Config.WorkingDirectory, nil, "docker container stop nginx")
+	}
+
+	renewCommand := "docker run --rm -i "
+	renewCommand += "-v \"/docker-nas/docker-data/letsencrypt:/etc/letsencrypt\" "
+	renewCommand += "-v \"/docker-nas/docker-data/certbot:/var/www/certbot\" "
+	renewCommand += "-p 80:80 "
+	renewCommand += "-p 443:443 "
+	renewCommand += "certbot/certbot 'certonly' '--standalone' "
+	renewCommand += "\"-d " + args[2] + "\" '--agree-tos' \"-m " + os.Getenv("GLOBAL_EMAIL") + "\""
+
+	util.Command(false, Config.WorkingDirectory, nil, renewCommand)
+
+	//Docker output was not empty so start it back up
+	if output != "" {
+		util.Command(false, Config.WorkingDirectory, nil, "docker container start nginx")
+	}
+
 	return ""
 }
 
 //TODO: Bring domain-remove the fully into golang
 func (a DomainModel) Delete() string {
-	// util.Command(false, Config.WorkingDirectory, nil, "./scripts/domain-remove.sh")
+
+	renewCommand := "docker run --rm -i "
+	renewCommand += "-v \"/docker-nas/docker-data/letsencrypt:/etc/letsencrypt\" "
+	renewCommand += "-v \"/docker-nas/docker-data/certbot:/var/www/certbot\" "
+	renewCommand += "certbot/certbot 'delete'"
+
+	util.Command(false, Config.WorkingDirectory, nil, renewCommand)
 	return ""
 }
