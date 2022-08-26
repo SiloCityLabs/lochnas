02c35ce1de96cdbc8b1f8449c6a26385e82548d5
Luis Rodriguez <luis@silocitylabs.com>
2022-08-26 13:45:05 -0400

    fixing collabora/nextcloud network issues

--- docker-templates/collabora/after-start.sh
+++ docker-templates/collabora/after-start.sh
@@ -8,11 +8,11 @@ if [[ $NEXTCLOUD_ENABLED -eq "true" ]]; then
     fi
 
     #Use username and password if set in the environment varibles for the url
-    if [[ ! -z "${username}" ]]; then
-        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://${username}:${password}@office.$GLOBAL_DOMAIN/ richdocuments wopi_url
-    else
-        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://office.$GLOBAL_DOMAIN/ richdocuments wopi_url
-    fi
+    # if [[ ! -z "${GLOBAL_COLLABORA_USERNAME}" ]]; then
+    #     docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://${GLOBAL_COLLABORA_USERNAME}:${GLOBAL_COLLABORA_PASSWORD}@office.$GLOBAL_DOMAIN richdocuments wopi_url
+    # else
+        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://office.$GLOBAL_DOMAIN richdocuments wopi_url
+    # fi
 
     docker exec -u $GLOBAL_UID -it nextcloud php occ richdocuments:activate-config
 fi
