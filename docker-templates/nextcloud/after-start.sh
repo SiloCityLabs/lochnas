6e07c0df6831a5b50c00398f63e009aa2a4194a1
Luis Rodriguez <luis@silocitylabs.com>
2022-08-27 11:51:10 -0400

    afterstart cant run on fresh nextcloud install

--- docker-templates/nextcloud/after-start.sh
+++ docker-templates/nextcloud/after-start.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# apparently nextcloud is doing integrity checks so i cant delete for now
-# Broken plugin
-# if [[ -d /docker-nas/docker-data/nextcloud/html/apps/files_trashbin ]]; then
-#     echo "Removing broken plugin 'Deleted Files' because it conflicts with External Storage"
-#     rm -rf /docker-nas/docker-data/nextcloud/html/apps/files_trashbin
-#     # This plugin causes 403 errors in ios/Android app when deleting files on External storage
-#     # files_trashbin could not be removed as it is a shipped app, so lets just delete it so we dont have to keep running this
-#     # docker exec -u $GLOBAL_UID -it nextcloud php occ app:delete files_trashbin
-# fi
+# Dont run if nextcloud has not been installed yet
+FILE=/docker-nas/docker-data/nextcloud/html/config/config.php
+if test -f "$FILE"; then
+    # apparently nextcloud is doing integrity checks so i cant delete for now
+    # Broken plugin
+    # if [[ -d /docker-nas/docker-data/nextcloud/html/apps/files_trashbin ]]; then
+    #     echo "Removing broken plugin 'Deleted Files' because it conflicts with External Storage"
+    #     rm -rf /docker-nas/docker-data/nextcloud/html/apps/files_trashbin
+    #     # This plugin causes 403 errors in ios/Android app when deleting files on External storage
+    #     # files_trashbin could not be removed as it is a shipped app, so lets just delete it so we dont have to keep running this
+    #     # docker exec -u $GLOBAL_UID -it nextcloud php occ app:delete files_trashbin
+    # fi
 
-docker exec -u $GLOBAL_UID -it nextcloud php occ app:disable files_trashbin
+    docker exec -u $GLOBAL_UID -it nextcloud php occ app:disable files_trashbin
 
-# Preview plugin
-if [[ ! -d /docker-nas/docker-data/nextcloud/html/custom_apps/previewgenerator ]]; then
-    echo "Installing preview cron plugin"
-    # rm -rf /docker-nas/docker-data/nextcloud/html/apps/files_trashbin
-    docker exec -u $GLOBAL_UID -it nextcloud php occ app:install previewgenerator
+    # Preview plugin
+    if [[ ! -d /docker-nas/docker-data/nextcloud/html/custom_apps/previewgenerator ]]; then
+        echo "Installing preview cron plugin"
+        # rm -rf /docker-nas/docker-data/nextcloud/html/apps/files_trashbin
+        docker exec -u $GLOBAL_UID -it nextcloud php occ app:install previewgenerator
 
-    echo "Run the following command once to pre-generate all thumbnails"
-    echo "docker exec -u $GLOBAL_UID -it nextcloud php occ preview:generate-all -vvv"
+        echo "Run the following command once to pre-generate all thumbnails"
+        echo "docker exec -u $GLOBAL_UID -it nextcloud php occ preview:generate-all -vvv"
+    fi
+
+    # docker exec -u $GLOBAL_UID -it nextcloud php occ maintenance:repair --include-expensive
+
+    # TODO: Possibly uninstall the following plugins:
+    # docker exec -u $GLOBAL_UID -it nextcloud php occ app:disable richdocuments
 fi
-
-# docker exec -u $GLOBAL_UID -it nextcloud php occ maintenance:repair --include-expensive
-
-# TODO: Possibly uninstall the following plugins:
-# docker exec -u $GLOBAL_UID -it nextcloud php occ app:disable richdocuments
 
 # bug in nginx cache http/s redirect infinit loop
 echo "Waiting..."
