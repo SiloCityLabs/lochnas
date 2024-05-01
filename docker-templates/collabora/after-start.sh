a274753fab916ad3f353c760b988fb2763408613
Luis Rodriguez <luis@silocitylabs.com>
2024-05-01 16:10:23 -0400

    fix after start

--- docker-templates/collabora/after-start.sh
+++ docker-templates/collabora/after-start.sh
@@ -8,11 +8,11 @@ if [[ $NEXTCLOUD_ENABLED -eq "true" ]]; then
     fi
 
     #Use username and password if set in the environment varibles for the url
-    # if [[ ! -z "${GLOBAL_COLLABORA_USERNAME}" ]]; then
-    #     docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://${GLOBAL_COLLABORA_USERNAME}:${GLOBAL_COLLABORA_PASSWORD}@office.$GLOBAL_DOMAIN richdocuments wopi_url
-    # else
-        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value https://office.$GLOBAL_DOMAIN richdocuments wopi_url
-    # fi
+    if [[ ! -z "${GLOBAL_COLLABORA_USERNAME}" ]]; then
+        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value http://${GLOBAL_COLLABORA_USERNAME}:${GLOBAL_COLLABORA_PASSWORD}@collabora:9980 richdocuments wopi_url
+    else
+        docker exec -u $GLOBAL_UID -it nextcloud php occ config:app:set --value http://collabora:9980 richdocuments wopi_url
+    fi
 
     docker exec -u $GLOBAL_UID -it nextcloud php occ richdocuments:activate-config
 fi
